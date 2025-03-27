bec955627955d65181a091f83b9aea9c37650c33
lyazidbmd <lyazidbmd@gmail.com>
2025-03-27 15:06:54 +0100

    Update png_test_decode.c

--- stegano/png_test_decode.c
+++ stegano/png_test_decode.c
@@ -6,166 +6,65 @@
 #include "pngwrap.h"
 #include "md5_utils.h"
 
-
 int main() 
 {
-    // Charger l'image modifiée contenant le message
-    bwimage_t *encoded_img = E3ACreateImage();
-    error_e err = E3ALoadImage("../lena_avec_codage.png", encoded_img);
+    // Charger l'image encodée
+    bwimage_t *img = E3ACreateImage();
+    error_e err = E3ALoadImage("../lena_avec_codage.png", img);
     if (err != E3A_OK) {
         fprintf(stderr, "Erreur : Impossible de charger l'image encodée\n");
         return EXIT_FAILURE;
     }
 
-    // Déterminer la capacité de l'image
-    unsigned int img_capacity = encoded_img->width * encoded_img->height;
+    // Créer un bitstream pour stocker les bits extraits
+    Bitstream *extracted_bs = CreateEmptyBitstream();
+    if (!extracted_bs) {
+        fprintf(stderr, "Erreur : Impossible de créer le bitstream\n");
+        E3AFreeImage(img);
+        return EXIT_FAILURE;
+    }
 
-    Bitstream *b=CreateEmptyBitstream();
-    for(int i=0; i<encoded_img->height; i++){
-        for(int j=0; j<encoded_img->width; j++){
-            WriteBit(b, encoded_img->data[i][j] & 1);
+    // Extraire les bits LSB de chaque pixel
+    for (unsigned int y = 0; y < img->height; y++) {
+        for (unsigned int x = 0; x < img->width; x++) {
+            // Accès sécurisé au pixel (adapté à votre structure 2D)
+            unsigned char pixel = img->data[y][x];
+            unsigned char lsb = pixel & 0x01;
+            WriteBit(extracted_bs, lsb);
         }
     }
 
-    unsigned int data_size=GetMessageSize(b->data);
+    // Convertir les bits en message
+    unsigned char *extracted_data = (unsigned char *)extracted_bs->data;
+    unsigned int extracted_size = GetMessageSize(extracted_data);
 
-    printf("Found data size: %d\n", data_size);
-    
-    // // Extraction de la taille du message stockée au début
-    // unsigned int msg_size = 0;
-    // for (int i = 0; i < 32; i++) { // Stocké sur 32 bits (4 octets)
-    //     msg_size |= (*encoded_img->data[i] & 1) << i;
-    // }
-
-    // if (msg_size * 8 > img_capacity) {
-    //     fprintf(stderr, "Erreur : Message corrompu ou trop grand\n");
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-
-    // // Allocation du message
-    // char *extracted_message = (char *)malloc(msg_size + 1);
-    // if (!extracted_message) {
-    //     fprintf(stderr, "Erreur : Impossible d'allouer la mémoire pour le message\n");
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-
-    // // Extraire les bits du message
-    // Bitstream *bs = CreateBitstreamOnData(extracted_message, msg_size);
-    // if (!bs) {
-    //     fprintf(stderr, "Erreur : Impossible de créer le bitstream\n");
-    //     free(extracted_message);
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-
-    // int bit_index = 32; // Début du message après la taille
-    // for (unsigned int i = 0; i < msg_size; i++) {
-    //     unsigned char byte = 0;
-    //     for (int bit = 0; bit < 8; bit++) {
-    //         byte |= (*encoded_img->data[bit_index++] & 1) << bit;
-    //     }
-    //     extracted_message[i] = byte;
-    // }
-    // extracted_message[msg_size] = '\0';
-
-    // // Vérification du hash MD5
-    // unsigned char md5_computed[MD5_DIGEST_LENGTH];
-    // calculate_md5((unsigned char*)extracted_message, msg_size - MD5_DIGEST_LENGTH, md5_computed);
+    // Vérifier la taille
+    if (extracted_size == 0 || extracted_size > (img->width * img->height / 8)) {
+        fprintf(stderr, "Erreur : Taille du message invalide\n");
+        FreeBitstream(extracted_bs);
+        E3AFreeImage(img);
+        return EXIT_FAILURE;
+    }
 
 
-    // if (memcmp(md5_computed, extracted_message + msg_size - MD5_DIGEST_LENGTH, MD5_DIGEST_LENGTH) != 0) 
-    // {
-    // fprintf(stderr, "Erreur : Hachage MD5 ne correspond pas, message corrompu\n");
-    // free(extracted_message);
-    // FreeBitstream(bs);
-    E3AFreeImage(encoded_img);
-    
-    
+    // Sauvegarder le message extrait
+    const char *filename = GetMessageFileName(extracted_data);
+    FILE *out = fopen(filename, "wb");
+    if (!out) {
+        fprintf(stderr, "Erreur : Impossible de créer %s\n", filename);
+        FreeBitstream(extracted_bs);
+        E3AFreeImage(img);
+        return EXIT_FAILURE;
+    }
 
+    fwrite(extracted_data + START_DATA, 1, extracted_size, out);
+    fclose(out);
 
-    // Sauvegarde du message décodé
-    // FILE *msg_file = fopen("decoded_message.txt", "w");
-    // if (!msg_file) {
-    //     fprintf(stderr, "Erreur : Impossible d'écrire le message\n");
-    //     free(extracted_message);
-    //     FreeBitstream(bs);
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-    // fprintf(msg_file, "%s", extracted_message);
-    // fclose(msg_file);
-    // printf("Message décodé avec succès et enregistré sous 'decoded_message.txt'\n");
+    printf("Message extrait avec succès dans %s\n", filename);
 
-    // // Reconstruction de l'image originale
-    // for (unsigned int i = 32; i < img_capacity; i++) {
-    //     *encoded_img->data[i] &= ~1;  // Réinitialiser le bit LSB à 0
-    // }
-    
-    // err = E3ADumpImage("../lena_reconstruite.png", encoded_img);
-    // if (err != E3A_OK) {
-    //     fprintf(stderr, "Erreur : Impossible de sauvegarder l'image reconstruite\n");
-    //     free(extracted_message);
-    //     FreeBitstream(bs);
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-
-    // printf("Image reconstruite avec succès sous 'lena_reconstruite.png'\n// Extraction de la taille du message stockée au début
-    // unsigned int msg_size = 0;
-    // for (int i = 0; i < 32; i++) { // Stocké sur 32 bits (4 octets)
-    //     msg_size |= (*encoded_img->data[i] & 1) << i;
-    // }
-
-    // if (msg_size * 8 > img_capacity) {
-    //     fprintf(stderr, "Erreur : Message corrompu ou trop grand\n");
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-
-    // // Allocation du message
-    // char *extracted_message = (char *)malloc(msg_size + 1);
-    // if (!extracted_message) {
-    //     fprintf(stderr, "Erreur : Impossible d'allouer la mémoire pour le message\n");
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-return EXIT_FAILURE;
-    // // Extraire les bits du message
-    // Bitstream *bs = CreateBitstreamOnData(extracted_message, msg_size);
-    // if (!bs) {
-    //     fprintf(stderr, "Erreur : Impossible de créer le bitstream\n");
-    //     free(extracted_message);
-    //     E3AFreeImage(encoded_img);
-    //     return EXIT_FAILURE;
-    // }
-
-    // int bit_index = 32; // Début du message après la taille
-    // for (unsigned int i = 0; i < msg_size; i++) {
-    //     unsigned char byte = 0;
-    //     for (int bit = 0; bit < 8; bit++) {
-    //         byte |= (*encoded_img->data[bit_index++] & 1) << bit;
-    //     }
-    //     extracted_message[i] = byte;
-    // }
-    // extracted_message[msg_size] = '\0';
-
-    // // Vérification du hash MD5
-    // unsigned char md5_computed[MD5_DIGEST_LENGTH];
-    // calculate_md5((unsigned char*)extracted_message, msg_size - MD5_DIGEST_LENGTH, md5_computed);
-
-
-    // if (memcmp(md5_computed, extracted_message + msg_size - MD5_DIGEST_LENGTH, MD5_DIGEST_LENGTH) != 0) 
-    // {
-    // fprintf(stderr, "Erreur : Hachage MD5 ne correspond pas, message corrompu\n");
-    // free(extracted_message);
-    // FreeBitstream(bs);");
-
-    // Libération de la mémoire
-    //free(extracted_message);
-    //FreeBitstream(bs);
-    // E3AFreeImage(encoded_img);
+    // Nettoyage
+    FreeBitstream(extracted_bs);
+    E3AFreeImage(img);
 
     return EXIT_SUCCESS;
 }
